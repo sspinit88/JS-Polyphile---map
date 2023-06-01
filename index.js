26e828c166cd46dc21c90f0d4a58f791d03ac778
sspinit88 <39233740+sspinit88@users.noreply.github.com>
2023-06-01 23:11:44 +0400

    1 file modified

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ const res2 = Array.prototype.myMap.call(
 console.log('2 - result:', res2);
 
 //// test 3
-// const res3 = Array.prototype.myMap.call('test', (value) => `${value},`);
-// console.log('3 - result:', res3);
+const res3 = Array.prototype.myMap.call('test', (value) => `${value},`);
+console.log('3 - result:', res3);
